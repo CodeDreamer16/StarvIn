b535e729441b0c6bd640a022901af58a697fd204
Rohan Vyas <129625888+CodeDreamer16@users.noreply.github.com>
2025-11-06 17:23:20 -0500

    Updated service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,23 @@
+// Basic offline + update service worker
+const CACHE_NAME = "vybin-cache-v1";
+const URLS_TO_CACHE = ["/", "/index.html", "/manifest.json"];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(URLS_TO_CACHE))
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => response || fetch(event.request))
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((names) =>
+      Promise.all(names.filter((n) => n !== CACHE_NAME).map((n) => caches.delete(n)))
+    )
+  );
+});
